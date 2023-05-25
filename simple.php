e9c70a82e6006f71fba444c6795a71e64f6aa614
SundayChidinmaJoy <133103849+SundayChidinmaJoy@users.noreply.github.com>
2023-05-25 11:30:58 +0100

    Create simple.php

---
+++ simple.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Sunday Chidinma Joy";
+$regnumber = "2021/HND/33216/CS";
+$soo = "Abia State";
+$topic = "Design and Deployment of Artificial Intelligent Powered E-commerce Website, A Case Study of Lizkon Clothings";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
